d18146e01419d84241289f1f15064b3ae8a23eb1
loris <loris.chastanet@gmail.com>
2021-04-12 19:46:02 +0200

    state and useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "../styles/App.css";
+import logo from "../assets/logo.png";
 import Banner from "./Banner";
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
 import Footer from "./Footer";
 
 function App() {
+  const [cart, updateCart] = useState([]);
+
   return (
     <div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
+      <Banner>
+        <img src={logo} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+      <div className="lmj-layout-inner">
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
+      </div>
       <Footer />
     </div>
   );
